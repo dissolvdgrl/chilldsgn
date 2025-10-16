a4f4ac496cf9c70ba6b17ef9ffa56801ea68e57d
Christie Hill <c@chilldsgn.com>
2025-10-16 05:47:26 +0200

    Add a new post

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 // Route::statamic('example', 'example-view', [
 //    'title' => 'Example'
 // ]);
+
+Route::get('/hi/test', function () {
+    return 'hello Kammi';
+});
